cc6600bb8df577d0b365ec9d7eb624ba1edef9e4
Yue Zhang <architectyuezhang@gmail.com>
2022-04-17 15:26:51 -0700

    add some assets

--- screens/Home.js
+++ screens/Home.js
@@ -2,6 +2,7 @@ import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import HeaderTab from '../components/HeaderTab'
 import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <HeaderTab />
         <SearchBar />
       </View>
+      <Categories />
     </SafeAreaView>
   )
 }
